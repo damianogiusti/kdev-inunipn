f75d62c7b86a009d4ce9db018f62ba4e57f94a08
Andrea Minato <andrea.minato@hotmail.it>
2017-06-26 09:33:32 +0200

    LoginPresenter created

---
+++ InUniPn/Classes/UI/Login/LoginPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  LoginPresenter.swift
+//  InUniPn
+//
+//  Created by Andrea Minato on 26/06/17.
+//  Copyright © 2017 KDev. All rights reserved.
+//
+
+import UIKit
+
+class LoginPresenter: BasePresenter {
+
+}
