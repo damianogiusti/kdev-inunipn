f4f7a4ba9c8951d858860593d2b439f96e19f980
Andrea Minato <andrea.minato@hotmail.it>
2017-06-26 09:31:37 +0200

    MVP folder e BasePresenter.swift

---
+++ InUniPn/Classes/UI/MVP/BasePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  BasePresenter.swift
+//  InUniPn
+//
+//  Created by Andrea Minato on 26/06/17.
+//  Copyright © 2017 KDev. All rights reserved.
+//
+
+import UIKit
+
+class BasePresenter: NSObject {
+
+}
