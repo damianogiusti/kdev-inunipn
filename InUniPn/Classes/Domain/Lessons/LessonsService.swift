b9b9faff6ee58af146c9f8c2a483d3e0fc82c439
Damiano Giusti <damiano.giusti@molo17.com>
2017-06-28 09:32:39 +0200

    Base lesson service

---
+++ InUniPn/Classes/Domain/Lessons/LessonsService.swift
@@ -0,0 +1,13 @@
+//
+//  LessonsService.swift
+//  InUniPn
+//
+//  Created by Damiano Giusti on 28/06/17.
+//  Copyright © 2017 KDev. All rights reserved.
+//
+
+import Foundation
+
+class LessonsService: BaseService {
+
+}
