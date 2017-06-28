4b38abc325d74154d3dc69d9993e24c7a0eeac48
Damiano Giusti <damiano.giusti@molo17.com>
2017-06-28 16:59:27 +0200

    news card cell storyboard

---
+++ InUniPn/Classes/UI/News/NewsTableViewCell/NewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NewsTableViewCell.swift
+//  InUniPn
+//
+//  Created by Damiano Giusti on 28/06/17.
+//  Copyright © 2017 KDev. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- InUniPn/Classes/UI/News/NewsViewController.swift
+++ InUniPn/Classes/UI/News/NewsViewController.swift
@@ -8,21 +8,21 @@
 
 import UIKit
 
-class NewsViewController:UITableViewController {
-    
-    ///associo il tab item al controller
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        
-        // Initialize Tab Bar Item
-        tabBarItem = UITabBarItem(title: "News", image: UIImage(named: "ios-book-outline"), tag: 0)
-    }
-    
+class NewsViewController: UITableViewController {
+
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var titleTextView: UILabel!
+    @IBOutlet weak var contentTextView: UILabel!
+
+    @IBOutlet weak var favoritesButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Initialize Tab Bar Item
+        tabBarItem = UITabBarItem(title: "News", image: #imageLiteral(resourceName: "ios-book-outline"), tag: 0)
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
