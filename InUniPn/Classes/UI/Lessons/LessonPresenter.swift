4ebf071a10af69a0eebc51d239e718a61d140a0f
Andrea Minato <andrea.minato@hotmail.it>
2017-06-28 10:42:44 +0200

    Created Classes

---
+++ InUniPn/Classes/UI/Lessons/LessonPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  LessonPresenter.swift
+//  InUniPn
+//
+//  Created by Andrea Minato on 28/06/17.
+//  Copyright © 2017 KDev. All rights reserved.
+//
+
+import UIKit
+
+class LessonPresenter: BasePresenter {
+
+}
---
+++ InUniPn/Classes/UI/Lessons/LessonView.swift
@@ -0,0 +1,15 @@
+//
+//  LessonView.swift
+//  InUniPn
+//
+//  Created by Andrea Minato on 28/06/17.
+//  Copyright © 2017 KDev. All rights reserved.
+//
+
+import UIKit
+
+protocol LessonView: BaseView {
+
+    
+    
+}
