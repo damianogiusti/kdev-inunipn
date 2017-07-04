1a5e59cdb63c6e7163337aec09b5a92283bdb3b1
Damiano Giusti <damiano.giusti@molo17.com>
2017-07-04 10:32:42 +0200

    Removed hardcoded numbers

--- InUniPn/Classes/UI/Main/TabBarController.swift
+++ InUniPn/Classes/UI/Main/TabBarController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 fileprivate struct ViewControllers {
     static let news = String(describing: NewsTableViewController.self)
+    static let newsPosition = 0
+
     static let lessons = String(describing: LessonsViewController.self)
+    static let lessonsPosition = 1
+
     static let profile = String(describing: ProfileViewController.self)
+    static let profilePosition = 2
 }
 
 class TabBarController: UITabBarController {
@@ -25,24 +30,24 @@ class TabBarController: UITabBarController {
         /// associo il controller delle news
         let newsStoryboard: UIStoryboard = UIStoryboard(name: "News", bundle: nil)
         let newsViewController = newsStoryboard.instantiateViewController(withIdentifier: ViewControllers.news)
-        self.viewControllers?.insert(newsViewController, at: 0)
-        self.tabBar.items?[0].title = Strings.news
-        self.tabBar.items?[0].image = #imageLiteral(resourceName: "ios-book-outline")
+        self.viewControllers?.insert(newsViewController, at: ViewControllers.newsPosition)
+        self.tabBar.items?[ViewControllers.newsPosition].title = Strings.news
+        self.tabBar.items?[ViewControllers.newsPosition].image = #imageLiteral(resourceName: "ios-book-outline")
         
         
         /// associo il controller degli orari
         let lessonsStoryboard: UIStoryboard = UIStoryboard(name: "Lessons", bundle: nil)
         let lessonsViewController = lessonsStoryboard.instantiateViewController(withIdentifier: ViewControllers.lessons)
-        self.viewControllers?.insert(lessonsViewController, at: 1)
-        self.tabBar.items?[1].title = Strings.lessons
-        self.tabBar.items?[1].image = #imageLiteral(resourceName: "ios-time-outline")
+        self.viewControllers?.insert(lessonsViewController, at: ViewControllers.lessonsPosition)
+        self.tabBar.items?[ViewControllers.lessonsPosition].title = Strings.lessons
+        self.tabBar.items?[ViewControllers.lessonsPosition].image = #imageLiteral(resourceName: "ios-time-outline")
 
         /// associo il controller del profilo
         let profileStoryboard: UIStoryboard = UIStoryboard(name: "Profile", bundle: nil)
         let profileViewController = profileStoryboard.instantiateViewController(withIdentifier: ViewControllers.profile)
-        self.viewControllers?.insert(profileViewController, at: 2)
-        self.tabBar.items?[2].title = Strings.profile
-        self.tabBar.items?[2].image = #imageLiteral(resourceName: "ios-person")
+        self.viewControllers?.insert(profileViewController, at: ViewControllers.profilePosition)
+        self.tabBar.items?[ViewControllers.profilePosition].title = Strings.profile
+        self.tabBar.items?[ViewControllers.profilePosition].image = #imageLiteral(resourceName: "ios-person")
     }
 
     override func didReceiveMemoryWarning() {
