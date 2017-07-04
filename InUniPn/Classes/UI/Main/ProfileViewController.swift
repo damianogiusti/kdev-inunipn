b78c74b2bac593658214c3b51598f9f66d200ce6
edward ilie <tsam013@tecnicosuperiorekennedy.it>
2017-07-04 11:43:17 +0200

    grafica del profilo sistemata. i settings noooo

--- InUniPn/Classes/UI/Main/ProfileViewController.swift
+++ InUniPn/Classes/UI/Main/ProfileViewController.swift
@@ -9,29 +9,34 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var NameSurnameLabel: UILabel!
+    @IBOutlet weak var UniversityLabel: UILabel!
+    @IBOutlet weak var UserImage: UIImageView!
     
-    ///associo il tab item al controller
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        
-        // Initialize Tab Bar Item
-        tabBarItem = UITabBarItem(title: "Profilo", image: UIImage(named: "ios-person"), tag: 2)
-    }
-    
+    @IBOutlet weak var segment: UISegmentedControl!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    }
 
+        UniversityLabel.textColor = UIColor.gray
+        NameSurnameLabel.textColor = UIColor.black
+        UserImage.layer.borderWidth = 0
+        UserImage.layer.borderWidth = 2
+        UserImage.layer.borderColor = UIColor.black.cgColor
+        UserImage.layer.masksToBounds = false
+        UserImage.layer.cornerRadius = UserImage.frame.height/2
+        UserImage.clipsToBounds = true
+        segment.setWidth(140, forSegmentAt: 0)
+        segment.setWidth(140, forSegmentAt: 1)
+        segment.backgroundColor = UIColor.gray
+        segment.tintColor = UIColor.init(red: 178, green: 34, blue: 34, alpha: 1)
+        
+        segment.apportionsSegmentWidthsByContent = true
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         appDelegate.tabBarController?.title = Strings.profile
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    
 }
+
