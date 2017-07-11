186218fbbf99eebefb9536634fb584e92f640782
Mattia <m.contin.dev@gmail.com>
2017-07-11 14:18:57 +0200

    Strings in italian

--- InUniPn/Localization/String.swift
+++ InUniPn/Localization/String.swift
@@ -12,85 +12,85 @@ import Foundation
 // swiftlint:disable type_name
 
 enum L10n {
-  /// Cancel
+  /// Annulla
   static let cancel = L10n.tr("Cancel")
-  /// Are you shure that you want to logout?
+  /// Sei sicuro di voler effettuare il logout?
   static let confirmLogoutMessage = L10n.tr("confirm logout message")
-  /// Confirm password
+  /// Conferma password
   static let confirmPassword = L10n.tr("Confirm password")
-  /// Repeat your password
+  /// Ripeti la password
   static let confirmPasswordPlaceholder = L10n.tr("Confirm password placeholder")
-  /// Contacting Facebook, please wait...
+  /// Mi sto collegando a Facebook, attendi...
   static let contactingFacebook = L10n.tr("contacting facebook")
-  /// Detail
+  /// Dettaglio
   static let detail = L10n.tr("Detail")
-  /// Email address
+  /// Indirizzo email
   static let email = L10n.tr("Email")
-  /// example@unipn.it
+  /// esempio@unipn.it
   static let emailPlaceholder = L10n.tr("Email placeholder")
-  /// Error
+  /// Errore
   static let error = L10n.tr("Error")
-  /// Unable to join this kind of lesson
+  /// Errore durante il salvataggio della lezione
   static let errorJoiningLessons = L10n.tr("Error joining lessons")
-  /// An error occourred while retrieving lessons, try again later
+  /// Si è verificato un errore durante il download delle lezioni, riprova più tardi
   static let errorRetrievingLessons = L10n.tr("Error retrieving lessons")
-  /// An error occourred while retrieving news, try again later
+  /// Si è verificato un errore durante il download delle news, riprova più tardi
   static let errorRetrievingNews = L10n.tr("Error retrieving news")
-  /// An error occourred while retrieving universities, try again later
+  /// Si è verificato un errore durante la selezione delle università, riprova più tardi
   static let errorRetrievingUniveristies = L10n.tr("Error retrieving univeristies")
-  /// An error occourred while saving
+  /// Si è verificato un errore durante il salvataggio delle informazioni
   static let errorSaving = L10n.tr("Error saving")
-  /// An error occourred while loggin in, try again later
+  /// Si è verificato un errore durante il login, riprova più tardi
   static let errorWhileLogginIn = L10n.tr("Error while loggin in")
-  /// An error occourred while signin up, try again later
+  /// Si è verificato un errore durante la registrazione, riprova più tardi
   static let errorWhileSigninUp = L10n.tr("Error while signin up")
-  /// Your event was added successfully
+  /// Evento salvato con successo!
   static let eventAdded = L10n.tr("event added")
   /// Please fill all the required fields
   static let fillAllFields = L10n.tr("Fill all fields")
-  /// You have successfully joined the lesson
+  /// Lezione salvata con successo
   static let joinedSuccessfully = L10n.tr("joined successfully")
-  /// Lessons
+  /// Lezioni
   static let lessons = L10n.tr("Lessons")
-  /// Notify for lessons
+  /// Notifica lezioni
   static let lessonsNotifications = L10n.tr("Lessons Notifications")
-  /// Lessons reminder
+  /// Tempo notifica lezioni
   static let lessonsReminderInterval = L10n.tr("Lessons Reminder Interval")
-  /// Loading, please wait...
+  /// Caricamento, attendi...
   static let loading = L10n.tr("loading")
   /// Logout
   static let logout = L10n.tr("Logout")
-  /// minutes
+  /// minuti
   static let minutePlural = L10n.tr("Minute plural")
-  /// minute
+  /// minuto
   static let minuteSingular = L10n.tr("Minute singular")
-  /// Name
+  /// Nome
   static let name = L10n.tr("Name")
   /// Mario Rossi
   static let namePlaceholder = L10n.tr("Name placeholder")
   /// News
   static let news = L10n.tr("News")
-  /// There are no planned lessons. A good opportunity to go sunbathing!
+  /// Non ci sono lezioni salvate. Un'ottima occasione per andare al mare!
   static let noLessonsPresent = L10n.tr("no lessons present")
-  /// It seems that you have no news. Let's stay tuned!
+  /// Non hai salvato alcuna news. Rimani aggiornato!
   static let noNewsPresent = L10n.tr("no news present")
-  /// I cannot perform this operation without your permission
+  /// Non è possibile usufruire di questa funzionalità senza i permessi
   static let noPermissionGiven = L10n.tr("No permission given")
-  /// Notifications
+  /// Notifiche
   static let notifications = L10n.tr("Notifications")
   /// Password
   static let password = L10n.tr("Password")
-  /// Select the university you are attending
+  /// Seleziona la tua università
   static let pickUniversity = L10n.tr("Pick university")
-  /// Profile
+  /// Profilo
   static let profile = L10n.tr("Profile")
-  /// Account settings
+  /// Impostazioni
   static let settings = L10n.tr("Settings")
-  /// University
+  /// Università
   static let university = L10n.tr("University")
-  /// You have successfully unjoined the lesson
+  /// Lezione dimenticata con successo
   static let unjoinedSuccessfully = L10n.tr("unjoined successfully")
-  /// Something went wrong, try again later
+  /// Si è verificato un errore, riprova più tardi
   static let unknownError = L10n.tr("Unknown error")
 }
 
